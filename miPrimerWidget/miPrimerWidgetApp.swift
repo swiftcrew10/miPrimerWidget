9dca771447b77b221c2284ab35d73ceed28f61a1
urnice1111 <emilianogarram2910@gmail.com>
2025-04-14 13:39:37 -0600

    Initial Commit

---
+++ miPrimerWidget/miPrimerWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  miPrimerWidgetApp.swift
+//  miPrimerWidget
+//
+//  Created by UwU on 14/04/25.
+//
+
+import SwiftUI
+
+@main
+struct miPrimerWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ miPrimerWidgetTests/miPrimerWidgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  miPrimerWidgetTests.swift
+//  miPrimerWidgetTests
+//
+//  Created by UwU on 14/04/25.
+//
+
+import Testing
+@testable import miPrimerWidget
+
+struct miPrimerWidgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
